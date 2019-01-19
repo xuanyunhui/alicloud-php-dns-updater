439f068af2c72757f647500efec56725e3713c9f
宣云辉 <yunhui@icloud.com>
2019-01-19 12:19:34 +0800

    Update index.php

--- index.php
+++ index.php
@@ -6,13 +6,14 @@ include_once 'alicloud-php-updaterecord/V20150109/AlicloudUpdateRecord.php';
 
 use Roura\Alicloud\V20150109\AlicloudUpdateRecord;
 
-$AccessKeyId     = 'ACCESS_KEY_ID';
-$AccessKeySecret = 'ACCESS_KEY_SECRET';
+$AccessKeyId     = getenv('ACCESS_KEY_ID');
+$AccessKeySecret = getenv('ACCESS_KEY_SECRET');
 $updater         = new AlicloudUpdateRecord($AccessKeyId, $AccessKeySecret);
+$domainname      = getenv('DOMAIN_NAME');
 
 $newIp = $_SERVER['REMOTE_ADDR']; // New IP
 
-$updater->setDomainName('DOMAIN.COM');
+$updater->setDomainName($domainname);
 $updater->setRecordType('A');
 $updater->setRR('@');
 $updater->setValue($newIp);
