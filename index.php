0de9ec4e59c2d4dec50ab07afef9355e0073c2a2
宣云辉 <yunhui@icloud.com>
2019-01-19 13:20:59 +0800

    Update index.php

--- index.php
+++ index.php
@@ -10,12 +10,13 @@ $AccessKeyId     = getenv('ACCESS_KEY_ID');
 $AccessKeySecret = getenv('ACCESS_KEY_SECRET');
 $updater         = new AlicloudUpdateRecord($AccessKeyId, $AccessKeySecret);
 $domainname      = getenv('DOMAIN_NAME');
+$record          = getenv('RECORD_NAME');
 
 $newIp = $_SERVER['REMOTE_ADDR']; // New IP
 
 $updater->setDomainName($domainname);
 $updater->setRecordType('A');
-$updater->setRR('@');
+$updater->setRR($record);
 $updater->setValue($newIp);
 
 print_r($updater->sendRequest());
